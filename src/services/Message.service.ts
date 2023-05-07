1cdf0dacac3e400f6732deedc3538fbc07a3d3a8
fatima2110 <fatimaamiri2030@gmail.com>
2023-05-07 20:55:43 +0000

    enable auth

--- src/services/Message.service.ts
+++ src/services/Message.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { MessageDTO } from 'src/models/MessageDTO';
+import { AuthService } from './auth-service.service';
 @Injectable({
   providedIn: 'root'
 })
@@ -15,25 +16,43 @@ export class MessageService {
 
 
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient, private authService:AuthService) { }
   getMessagesRecepteur(id: number): Observable<MessageDTO[]> {
+    const token = this.authService.getToken();
+    const httpOptions = {
+      headers: {
+        "Authorization": "Bearer " + token
+      }
+    };
     //return this.httpClient.get<Fournisseur[]>(this.API_URL+this.ENDPOINT_fournisseurs)
     const url = `${this.API_URL}${this.message}/${id}`;
-    var Messages = this.httpClient.get<MessageDTO[]>(url);
+    var Messages = this.httpClient.get<MessageDTO[]>(url, httpOptions);
 
     console.log(Messages);
     return Messages;
   }
   getMessages(msg: MessageDTO): Observable<MessageDTO[]> {
+    const token = this.authService.getToken();
+    const httpOptions = {
+      headers: {
+        "Authorization": "Bearer " + token
+      }
+    };
     //return this.httpClient.get<Fournisseur[]>(this.API_URL+this.ENDPOINT_fournisseurs)
     const url = `${this.API_URL}${this.msgs}`;
-    var Message = this.httpClient.post<MessageDTO[]>(url, msg);
+    var Message = this.httpClient.post<MessageDTO[]>(url, msg, httpOptions);
     console.log(Message);
     return Message;
   }
   envoyerMessage(msg: MessageDTO): Observable<void> {
+    const token = this.authService.getToken();
+    const httpOptions = {
+      headers: {
+        "Authorization": "Bearer " + token
+      }
+    };
     const url = `${this.API_URL}${this.envy}`;
-    var Message = this.httpClient.post<void>(url, msg);
+    var Message = this.httpClient.post<void>(url, msg, httpOptions);
     console.log(Message);
     return Message;
 
